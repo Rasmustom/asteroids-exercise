af163baab5fb060342e0cefeaee7b1c70bbd95cb
Rasmus Tomsen <RTomsen@perforce.com>
2023-02-06 13:34:21 +0200

    added favicon

--- js/classes/MovingObject.js
+++ js/classes/MovingObject.js
@@ -16,19 +16,6 @@ export default class MovingObject {
         Canvas.drawCircle({ x: this.pos.x, y: this.pos.y, radius: 20 });
     }
 
-    static createRandom() {
-        const asd = new MovingObject(
-            { x: this.getRandInt(0, 500), y: this.getRandInt(0, 500) },
-            { x: this.getRandInt(-5, 5), y: this.getRandInt(-5, 5) }
-        );
-        // console.log(asd.pos, asd.vel);
-        return asd;
-    }
-
-    static getRandInt(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
     isOutOfBounds() {
         return this.pos.x < 0 || this.pos.x > 500 || this.pos.y < 0 || this.pos.y > 500;
     }
