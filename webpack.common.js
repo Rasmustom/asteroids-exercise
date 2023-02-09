da59f7b12a3f247bd9d2d984215d5176e0e589a5
Rasmus Tomsen <RTomsen@perforce.com>
2023-02-09 22:02:27 +0200

    webpack css

--- webpack.common.js
+++ webpack.common.js
@@ -23,7 +23,12 @@ module.exports = {
         },
     },
     module: {
-        rules: [],
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
     cache: false,
 };
